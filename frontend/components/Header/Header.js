f05bccc801d01e08b80122d701b8ce00dbe4da61
Sebastian <segurasebastian7887@gmail.com>
2018-11-26 20:50:15 -0300

    .gitignore updated

---
+++ frontend/components/Header/Header.js
@@ -0,0 +1,16 @@
+import Nav from '../Nav/Nav';
+
+const Header = () => (
+  <div>
+    <div>
+      <a href="">RGZ</a>
+      <Nav />
+    </div>
+    <div>
+      <p>Search</p>
+    </div>
+    <div>Cart</div>
+  </div>
+)
+
+export default Header;
---
+++ frontend/config.js
@@ -0,0 +1,2 @@
+export const endpoint = `http://localhost:8081`;
+export const perPage = 4;
