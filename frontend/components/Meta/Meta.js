cc87fc0c7ef95fc0c5a0e4580e9183598007f6f1
Sebastian <segurasebastian7887@gmail.com>
2018-11-27 00:10:46 -0300

    favicon added

--- frontend/components/Meta/Meta.js
+++ frontend/components/Meta/Meta.js
@@ -4,6 +4,7 @@ const Meta = () => (
   <Head>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charset="utf-8" />
+    <link rel="shortcut icon" href="/static/images/favicon.png" />
     <link rel="stylesheet" type="text/css" href="/static/css/nprogress.css" />
     <title>RGZ</title>
   </Head>
