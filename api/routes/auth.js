042c3b06ace4f99114d3f36f2e4283f71e105054
Seba <segurasebastian7887@gmail.com>
2018-11-28 11:10:28 -0300

    api: routes/auth.js removed

--- api/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const register = require('../controllers/register');
-
-router.post('/signup', register.handleRegister);
-
-module.exports = router;
