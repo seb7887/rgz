54d09f6f26eaef44536b4ae54ae7e2e68493b7b8
Sebastian <segurasebastian7887@gmail.com>
2018-12-11 21:54:38 -0300

    working on search autocomplete

---
+++ frontend/components/Search/Search.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Downshift, { resetIdCounter } from 'downshift';
+import Router from 'next/router';
+import debounce from 'lodash.debounce';
+
+class Autocomplete extends React.Component {
+  render() {
+    return (
+      Hello
+    );
+  }
+}
+
+export default Autocomplete;
